fff4d8a043ac86c1d5a306781c82470724d5c3cd
Your Name <you@example.com>
2018-01-29 15:45:12 +0800

    add logstash, but runing failed

--- php-fpm/5.6/php56.sh
+++ php-fpm/5.6/php56.sh
@@ -5,7 +5,7 @@ docker run \
 	-v /www/huanle:/www/huanle \
 	-v /www/self:/www/self \
 	--net=phpweb_appnet \
-	--ip 172.19.0.6 \
+	--ip 172.19.0.20 \
 	--name php56 \
 	-d registry.cn-hangzhou.aliyuncs.com/listen/php-fpm:5.6
 

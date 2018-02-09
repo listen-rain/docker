627470ab88474181e87a82f144201daccf08c0d6
wei <wei@example.com>
2018-02-09 19:23:06 +0800

    edit composer/install.sh

--- compose/install.sh
+++ compose/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/local/bin/docker-compose \
-	&& chmod +x /usr/local/bin/docker-compose \
-	&& docker-compose --help
+curl -L https://github.com/docker/compose/releases/download/1.8.0/run.sh > /usr/bin/docker-compose \
+        && chmod +x /usr/bin/docker-compose \
+        && docker-compose --help
 
--- composer/install.sh
+++ composer/install.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-	touch /usr/local/bin/composer
-	
-	echo '#!/bin/bash
+touch /usr/bin/composer \
+ && echo '#!/bin/bash
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 echo "Current working directory: '"$(pwd)"'"
-docker run --rm -v $(pwd):/app -v ~/.ssh:/root/.ssh composer/composer $@' > /usr/local/bin/composer
+docker run --rm -v $(pwd):/app -v ~/.ssh:/root/.ssh composer/composer $@' > /usr/bin/composer \
+ && chmod +x /usr/bin/composer \
+ && composer --help
 
-	chmod +x /usr/local/bin/composer
-
-	composer --help

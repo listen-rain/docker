fd5dfd502d63abb3dd47c4ed7542efbef85fbe20
wei <m15275049388@163.com>
2018-04-13 16:50:36 +0000

    edit mynode.sh

--- node/mynode.sh
+++ node/mynode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --name mynode -p 3000:3000 -p 4000:4000 -v /www:/www -d registry.cn-hangzhou.aliyuncs.com/listen/node:latest bash
+docker run -it --name mynode -p 3000:3000 -p 4000:4000 -v /www:/www -d registry.cn-hangzhou.aliyuncs.com/listen/node:latest bash
 

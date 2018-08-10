71756fc61761908843a8bf4559d676a03f99df50
zhu fengwei <m15275049388@163.com>
2018-08-10 10:20:34 +0800

    update docker-compose.yml

--- run/stop.sh
+++ run/stop.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-for container in $(docker ps | awk '{print $1}' | grep -v 'CONTAINER')
+all=""
+
+if [[ $2 == "a" ]]
+then 
+	all="-a"	
+fi
+
+containers=$(docker ps $all | awk '{print $1}' | grep -v 'CONTAINER')
+
+for container in $containers
 do
 	docker stop $container
+	
+	if [[ $1 == "rm" ]]
+	then
+		echo $container
+		docker rm -v $container
+	fi	
 done
 
 exit

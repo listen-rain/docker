4cf43a28efdf79092af531af991905c44f6a1174
wei <m15275049388@163.com>
2018-12-19 13:41:34 +0800

    update hexo

--- node/Hexo/start.sh
+++ node/Hexo/start.sh
@@ -3,14 +3,15 @@
 HEXO_WORKDIR='/www/hexo'
 
 if [ ! -d "$HEXO_WORKDIR" ];then
-    mkdir $HEXO_WORKDIR \
-	&& cd $HEXO_WORKDIR \
-	&& cnpm install hexo-cli -g 
-        
+	mkdir $HEXO_WORKDIR \
+		&& cd $HEXO_WORKDIR \
+		&& cnpm install hexo-cli -g  
+fi
+
 if [ ! -d "$HEXO_WORKDIR/blog" ];then
-    hexo init blog \
-	&& cd blog \
-	&& cnpm install
+	hexo init blog \
+	  && cd blog \
+	  && cnpm install
 fi
 
 # hexo server

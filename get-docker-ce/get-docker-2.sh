66f1b90d16c4636cd689f90850224414f2f7def8
listen <zhufengwei@aliyun.com>
2019-01-10 05:52:42 +0800

    update get-docker-2.sh

--- get-docker-ce/get-docker-2.sh
+++ get-docker-ce/get-docker-2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+
+if [[ -f get-docker.sh ]];then
+	
+fi
 sh get-docker.sh --mirror Aliyun \
+ && mkdir -p /etc/docker \
  && touch /etc/docker/daemon.json \
  && echo "{ \"registry-mirrors\": [ \"https://$1.mirror.aliyuncs.com\" ] }" > /etc/docker/daemon.json \
  && systemctl enable docker \

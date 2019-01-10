75214c4db0807b96c5856f861c057f05608715b3
zhufengwei <zhufengwei@100tal.com>
2019-01-10 16:04:28 +0800

    update get-docker-2 shell

--- get-docker-ce/get-docker-2.sh
+++ get-docker-ce/get-docker-2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [[ -f get-docker.sh ]];then
-    echo "正在安装...\n" \
+    echo "正在安装..." \
 	    && sh get-docker.sh --mirror Aliyun \
 	    && systemctl enable docker \
 	    && docker info
 fi
 
 if [[ -n $1 ]];then
-    echo "正在更换为国内镜像...\n" \
+    echo "正在更换为国内镜像..." \
         && mkdir -p /etc/docker \
         && touch /etc/docker/daemon.json \
         && echo "{ \"registry-mirrors\": [ \"https://$1.mirror.aliyuncs.com\" ] }" > /etc/docker/daemon.json \

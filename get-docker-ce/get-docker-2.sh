e7a2c08983ecc5a7abfcd8bf5de21b2e7995cc37
root <root@centos764desktop.localdomain>
2019-01-12 10:18:58 +0800

    update get-docker

--- get-docker-ce/get-docker-2.sh
+++ get-docker-ce/get-docker-2.sh
@@ -2,16 +2,19 @@
 
 if [[ -f get-docker.sh ]];then
     echo "正在安装..." \
-	    && sh get-docker.sh --mirror Aliyun \
-	    && systemctl enable docker \
-	    && docker info
+            && sh get-docker.sh --mirror Aliyun \
+            && sudo systemctl enable docker \
+            && sudo systemctl start docker \
+	    && sudo docker info
 fi
 
 if [[ -n $1 ]];then
     echo "正在更换为国内镜像..." \
-        && mkdir -p /etc/docker \
+        && sudo mkdir -p /etc/docker \
+	&& sudo chmod -R 777 /etc/docker \
         && touch /etc/docker/daemon.json \
         && echo "{ \"registry-mirrors\": [ \"https://$1.mirror.aliyuncs.com\" ] }" > /etc/docker/daemon.json \
-        && systemctl start docker \
-        && docker info
+        && sudo systemctl restart docker \
+        && sudo docker info
 fi
+

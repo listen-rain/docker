6f19929a016cbb834789c853f6c20062c9cf61b9
listen <zhufengwei@aliyun.com>
2019-01-13 22:30:40 +0800

    update node run bash

--- node/mynode.sh
+++ node/mynode.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# docker run -it --name mynode -p 3000:3000 -p 4000:4000 -v /www:/www -d registry.cn-hangzhou.aliyuncs.com/listen/node:latest bash
-
 docker run -it \
         --name mynode \
         -p 4000:4000 \
         -v /www:/www \
+	--restart always \	
         -d node bash
-
 
